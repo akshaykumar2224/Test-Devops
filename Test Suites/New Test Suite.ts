94a1bc88a3f55dca3d84816ae5a4b893a021102d
Shashi T <shashi.t@slkgroup.com>
2020-02-03 16:05:42 +0530

    commit on 2/3

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8a2ea506-e918-4a8b-afe7-afc5921d904a</testSuiteGuid>
+   <testCaseLink>
+      <guid>75ae8086-e55e-4243-9b73-ec1f43c4b658</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
